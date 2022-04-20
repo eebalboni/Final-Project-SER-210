bf69c7a1cb1a4c48f4c625727fb5f533720ceaae
amberk887 <amber.kusma@quinnipiac.edu>
2022-04-20 16:56:09 -0400

    database comments

--- app/src/main/java/edu/quinnipiac/ser210/finalproject/MainActivity.java
+++ app/src/main/java/edu/quinnipiac/ser210/finalproject/MainActivity.java
@@ -10,5 +10,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        // retrieve expiration date from database
+        //cursor.readDatabase():
+        //String[]columns (FoodDB.NAMW, FoodDB.EXPDATE);
+        //int[]
     }
 }
